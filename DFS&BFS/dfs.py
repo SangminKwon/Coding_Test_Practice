b21ba7527d505ec55f79db7cecb88d1d8117fe8a
SangminKwon <ksm9905@gmail.com>
2021-06-28 23:37:10 +0900

    2021.06.28 DFS 일부 구현 커밋

---
+++ DFS&BFS/dfs.py
@@ -0,0 +1,13 @@
+# DFS 구현
+
+def dfs(graph, v, visited):
+    # 현재 노드를 방문처리
+    visited[v] = True
+    print(v, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(인접 리스트 사용)
+graph =
